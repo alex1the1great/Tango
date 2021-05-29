02ab1814839e3e4c4b9ef3fd2c718100d129f64e
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-29 21:16:21 +0545

    Create about page

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Rango says hey there partner!')
+    message = 'Rango says hey there partner! <br><a href="/rango/about/">About</a>'
+    return HttpResponse(message)
+
+
+def about(request):
+    message = 'Rango says here is the about page.<br><a href=/rango/>Home</a>'
+    return HttpResponse(message)
