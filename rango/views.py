05b81f045c731ea6472841700c4e300e9c8c072c
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-30 12:51:59 +0545

    Add templates and static files

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    message = 'Rango says hey there partner! <br><a href="/rango/about/">About</a>'
-    return HttpResponse(message)
+    context = {
+        'message': 'Crunchy, creamy, cookie, candy, cupcake!'
+    }
+    return render(request, 'rango/index.html', context)
 
 
 def about(request):
